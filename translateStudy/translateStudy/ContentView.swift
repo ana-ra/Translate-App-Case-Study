c443101981dac075b5968115fb145acbad912b22
Raphael Kitahara <raphael_kitahara@hotmail.com>
2023-05-22 10:14:31 -0300

    test commit

--- translateStudy/translateStudy/ContentView.swift
+++ translateStudy/translateStudy/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Raphaa!")
+            Text("Hello, Raphaaa!")
         }
         .padding()
     }
