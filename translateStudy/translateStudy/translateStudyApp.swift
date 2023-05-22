af66b57987622f5ebbe4cfa37c57b8c6aaea0050
Silvana Rodrigues Alves <sil@10-40-48-73.0200.ecps.br.iosda.org>
2023-05-22 10:05:42 -0300

    first commit at the project

---
+++ translateStudy/translateStudy/translateStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  translateStudyApp.swift
+//  translateStudy
+//
+//  Created by Silvana Rodrigues Alves on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct translateStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
