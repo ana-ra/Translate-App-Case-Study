dea4f94ed86017e1caab5e400233eb6e02f41ec5
Luiza Souza <luizasouza@10-40-48-144.0200.ecps.br.iosda.org>
2023-05-22 10:04:21 -0300

    cria o projeto

---
+++ TranslateCopyCat/TranslateCopyCat/TranslateCopyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateCopyCatApp.swift
+//  TranslateCopyCat
+//
+//  Created by Luiza Souza on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TranslateCopyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
