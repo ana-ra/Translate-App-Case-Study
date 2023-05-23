5fba8eb994e847df9ec8c947bf09bbdb30e4a478
Pedro Ceccon <p247327@dac.unicamp.br>
2023-05-23 12:29:48 -0300

    API and fetch language

---
+++ translateStudy/translateStudy/TranslationAPI/TranslationAPI.swift
@@ -0,0 +1,40 @@
+//
+//  TranslationAPI.swift
+//  translateStudy
+//
+//  Created by Pedro Ceccon on 23/05/23.
+//
+
+import Foundation
+
+
+enum TranslationAPI {
+    case detectLanguage
+    case translate
+    case supportedLanguages
+
+    func getURL() -> String {
+        var urlString = ""
+ 
+        switch self {
+        case .detectLanguage:
+            urlString = "https://translation.googleapis.com/language/translate/v2/detect"
+ 
+        case .translate:
+            urlString = "https://translation.googleapis.com/language/translate/v2"
+ 
+        case .supportedLanguages:
+            urlString = "https://translation.googleapis.com/language/translate/v2/languages"
+        }
+ 
+        return urlString
+    }
+    
+    func getHTTPMethod() -> String {
+            if self == .supportedLanguages {
+                return "GET"
+            } else {
+                return "POST"
+            }
+        }
+}
